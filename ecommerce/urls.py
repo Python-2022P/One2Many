47e5b8f5b32aeba4cf1d8b8720d30b3d6c398d08
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-10 16:25:29 +0500

    companies;

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import CompanyView,get_company_id,get_product, get_product_id
+from .views import CompanyView
 
 
 urlpatterns = [
     path('',CompanyView.as_view()),
-    path('<int:id>',get_company_id),
-    path("<int:id>/products",get_product),
-    path('<int:company_id>/produts/<int:id>',get_product_id)
 ]
